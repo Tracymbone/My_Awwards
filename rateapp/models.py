12f0ba61efcba5a2fdaa07d9c10600ced8c5f861
moringa <tressy.mbone@student.moringaschool.com>
2022-03-22 23:35:28 +0300

    EDITED MODELS.PY

--- rateapp/models.py
+++ rateapp/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from pyuploadcare.dj.models import ImageField
-from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
+# from pyuploadcare.dj.models import ImageField
+# from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
   user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
-  bio = HTMLField()
-  profile_pic = ImageField(manual_crop ='1080x1080')
+  bio = CloudinaryField()
+  profile_pic = CloudinaryField(manual_crop ='1080x1080')
   contact_info = models.CharField(max_length=144)
 
   @classmethod
